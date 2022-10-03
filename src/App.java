afadf1f27db44a9df7ea37237744660f10644d76
Haroune Hadid <harounehadidbusiness@gmail.com>
2022-10-03 23:19:28 +0100

    Add more standard folders and files

--- src/App.java
+++ src/App.java
@@ -1,9 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+
 /**
  * App
  */
 public class App {
 
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello");
+
     }
 }
