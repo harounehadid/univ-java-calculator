138dbad847cc170e69bebb5532514664edc6151e
Haroune Hadid <harounehadidbusiness@gmail.com>
2022-10-04 22:45:21 +0100

    Display a frame

--- src/App.java
+++ src/App.java
@@ -1,18 +1,30 @@
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.BorderFactory;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 /**
  * App
  */
 public class App {
 
-    JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
+    public App() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
 
-    
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Java Test Frame");
+        frame.pack();
+        frame.setVisible(true);
+    }    
     
     public static void main(String[] args) {
-
+        new App();
     }
 }
